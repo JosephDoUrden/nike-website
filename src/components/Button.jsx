74ba9a62f7b0c0825c48bb489d8972b0887fc6c3
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-21 10:48:11 +0300

    Hero Section & ShoeCard etc.

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({ label, iconUrl }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red border-coral-red text-white rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red transition-all">
+      {label}
+      <img src={iconUrl} alt="icon" className="ml-2 rounded-full w-5 h-5" />
+    </button>
+  );
+};
+
+export default Button;
