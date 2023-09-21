ec636f02188d1b641f7faefee963f491a9afe188
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2023-09-21 15:27:55 +0300

    Speacial Offer Section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,22 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red border-coral-red text-white rounded-full hover:bg-white hover:text-coral-red hover:border-coral-red transition-all">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${
+        fullWidth && "w-full"
+      } hover:bg-white hover:text-coral-red hover:border-coral-red transition-all`}
+    >
       {label}
       {iconUrl && (
         <img src={iconUrl} alt="icon" className="ml-2 rounded-full w-5 h-5" />
